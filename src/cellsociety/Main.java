3a6ca74f0e39ffd919a50a1512a6bcc36a1e5ae1
Nick Strauch <njs35@duke.edu>
2021-10-15 10:40:07 -0400

    Created package layout for the View

--- src/cellsociety/Main.java
+++ src/cellsociety/Main.java
@@ -2,20 +2,20 @@ package cellsociety;
 
 
 /**
- * Feel free to completely change this code or delete it entirely. 
+ * Feel free to completely change this code or delete it entirely.
  */
 public class Main {
     /**
      * A method to test (and a joke :).
      */
-    public double getVersion () {
+    public double getVersion() {
         return 0.001;
     }
 
     /**
      * Start of the program.
      */
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello world");
     }
 }

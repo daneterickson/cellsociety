2b397aa8fe5b71f896ef2f43fec63c05d18b5fe2
Nick Strauch <njs35@duke.edu>
2021-10-15 21:42:56 -0400

    Got Google formatting running again.

--- src/cellsociety/Main.java
+++ src/cellsociety/Main.java
@@ -5,17 +5,18 @@ package cellsociety;
  * Feel free to completely change this code or delete it entirely.
  */
 public class Main {
-    /**
-     * A method to test (and a joke :).
-     */
-    public double getVersion() {
-        return 0.001;
-    }
 
-    /**
-     * Start of the program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
+  /**
+   * A method to test (and a joke :).
+   */
+  public double getVersion() {
+    return 0.001;
+  }
+
+  /**
+   * Start of the program.
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+  }
 }

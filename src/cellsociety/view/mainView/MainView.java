b9db409dcf8a25531754e595eb503a671ed093c1
Nick Strauch <njs35@duke.edu>
2021-10-17 22:33:14 -0400

    Reverted code back to state without the weird root.

--- src/cellsociety/view/mainView/MainView.java
+++ src/cellsociety/view/mainView/MainView.java
@@ -9,26 +9,15 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
 
-public class MainView {
+public class MainView extends BorderPane{
   GridView myGridView = new GridView();
-  private static final String DEFAULT_STYLESHEET = "/cellsociety/view/resources/Styling.css";
   SimControl mySimControl = new SimControl();
 
   public MainView(){
+    this.setCenter(myGridView.getGridCanvas());
+    this.setBottom(mySimControl.getSimControl());
   }
 
-
-  public Scene makeScene(int width, int height){
-    BorderPane root = new BorderPane();
-    root.setCenter(myGridView.getGridCanvas());
-    //root.setLeft();
-    //root.setRight();
-    //root.setTop();
-    //root.setBottom();
-    Scene scene = new Scene(root, width, height);
-    scene.getStylesheets().add(getClass().getResource(DEFAULT_STYLESHEET).toExternalForm());
-    return scene;
-  }
   /**
    * Updates the canvas (grid) in the view.
    */

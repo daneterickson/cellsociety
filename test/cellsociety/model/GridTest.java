188446c6d5b7e4d2fb383b2fd6089ee85028a7c7
Dane Erickson <dte12@duke.edu>
2021-10-21 18:22:36 -0400

    added more tests for Grid and made Controller handle File Exceptions instead of Parser

--- test/cellsociety/model/GridTest.java
+++ test/cellsociety/model/GridTest.java
@@ -3,6 +3,9 @@ package cellsociety.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import cellsociety.model.cell.GameOfLifeCell;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,13 +13,15 @@ public class GridTest {
 
   private Grid myGrid;
   private int myStates[][];
+  private ArrayList<ArrayList<Integer>> myStatesList;
   private int numRows;
   private int numCols;
 
   @BeforeEach
-  void setUp () {
+  void setUp() {
     myStates = new int[][]{{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 1, 1, 1, 0}, {0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0}};
+    myStatesList = new ArrayList<>();
     numRows = 5;
     numCols = 5;
     String type = "GameOfLife";
@@ -25,21 +30,27 @@ public class GridTest {
   }
 
   @Test
-  void testSetStartStates () {
-    for (int row=0; row<numRows; row++) {
-      for (int col=0; col<numCols; col++) {
-        assertEquals(myStates[row][col], myGrid.getCellStateNumber(row,col));
+  void testSetStartStates() {
+    for (int row = 0; row < numRows; row++) {
+      for (int col = 0; col < numCols; col++) {
+        assertEquals(myStates[row][col], myGrid.getCellStateNumber(row, col));
       }
     }
   }
 
   @Test
-  void testCellType () {
-    for (int row=0; row<numRows; row++) {
-      for (int col=0; col<numCols; col++) {
-        assertEquals(GameOfLifeCell.class, myGrid.getCell(row,col).getClass());
+  void testCellType() {
+    for (int row = 0; row < numRows; row++) {
+      for (int col = 0; col < numCols; col++) {
+        assertEquals(GameOfLifeCell.class, myGrid.getCell(row, col).getClass());
       }
     }
+  }
+
+  @Test
+  void testChangeState() {
+    myGrid.updateCell(0, 0, 1);
+    assertEquals(1, myGrid.getCellStateNumber(0, 0));
   }
 
 //  @Test

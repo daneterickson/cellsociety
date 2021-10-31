6417aaf33e4c3b6c5ed04bebf18d2ce17a309e82
Dane Erickson <dte12@duke.edu>
2021-10-31 15:02:07 -0400

    started making triangleGridView

--- src/cellsociety/view/center/TriangleGridView.java
+++ src/cellsociety/view/center/TriangleGridView.java
@@ -1,5 +1,37 @@
 package cellsociety.view.center;
 
-public class TriangleGridView {
+import cellsociety.controller.Controller;
+import cellsociety.view.left.CellProperties;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.transform.NonInvertibleTransformException;
 
+public class TriangleGridView extends GridView {
+
+  public TriangleGridView(CellProperties cellProps, Controller controller) {
+    super(cellProps, controller);
+  }
+
+  @Override
+  protected void findOptimalGridSizing(int numRows, int numCols) {
+
+  }
+
+  @Override
+  protected void updateCellColors(GraphicsContext gc) {
+
+  }
+
+  @Override
+  protected void drawGridLines(GraphicsContext gc) {
+    gc.setStroke(GRID_LINE_COLOR);
+    gc.setLineWidth(GRID_LINE_SIZE);
+    for (int j = 0; j < getNumRows(getCurrentGridNum()) + 1; j++) {
+      gc.strokeLine(0, j, getNumCols(getCurrentGridNum()), j);
+    }  }
+
+  @Override
+  protected void getMousePosOnGrid(MouseEvent mouseEvent) throws NonInvertibleTransformException {
+
+  }
 }
